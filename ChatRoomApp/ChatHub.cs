ac11784d72aa754e6314865ae38a2ba6f89e7ce3
alexf-dev <regalex82@gmail.com>
2020-06-11 07:39:00 +0600

    Create project

---
+++ ChatRoomApp/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoomApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+
+        public async Task AddMessage(string name, string message)
+        {
+            Console.WriteLine("Hub AddMessage {0} {1}\n", name, message);
+            await this.Clients.All.SendAsync("Send", message);
+        }
+    }
+}
